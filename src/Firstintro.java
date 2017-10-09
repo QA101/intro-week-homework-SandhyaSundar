73fe4bde3c3084cdea8e941b437a8f61ee6fdd3f
Sandhya <sundarsandhya6@gmail.com>
2017-10-08 20:29:34 -0700

    Home week 1

---
+++ src/Firstintro.java
@@ -0,0 +1,19 @@
+
+public class Firstintro {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("I am Sandhya born and brought up in India");
+		System.out.println("I was born in southern part of India called Tamil Nadu");
+		System.out.println("My childhood filled with beautiful memories with my two siblings");
+	    System.out.println("I was a agricultrue graduate");
+	    System.out.println("I was working in Bank as 'Asst Manager'");
+	    System.out.println("I came to US in 2012");
+	    System.out.println("I am proud mom of Twins having great fun with them");
+	    System.out.println("Now I want to persue my career after 5 years");
+	    System.out.println("Heard about QA101 and learnt software testing ");
+	    System.out.println("Right now doing my second course with QA101");
+	
+	}
+
+}
